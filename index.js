86bfefb0d8859d4569b70eb43b32fbb0a1986e4a
ZubayerAhammedTolon <zs.tolon0073@gmail.com>
2022-08-03 19:37:00 +0600

    payment api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +13,7 @@ app.use(express.json());
 //steps-2
 //from database > connect > connect your application > click checkbox > from that code
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.gbrle.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -70,6 +71,19 @@ async function run() {
                 res.status(403).send({ message: 'Forbidden' });
             }
         }
+
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
+
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -161,6 +175,13 @@ async function run() {
             else {
                 return res.status(403).send({ message: 'Forbidden access' })
             }
+        })
+
+        app.get('/booking/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking);
         })
 
         app.post('/booking', async (req, res) => {
